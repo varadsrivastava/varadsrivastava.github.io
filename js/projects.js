063af0ecef07d360034f3738b27891b634cc02d9
VARAD SRIVASTAVA <varad.srivastava26@gmail.com>
2023-10-25 14:44:48 +0530

    made project faster, update to failure

--- js/projects.js
+++ js/projects.js
@@ -48,18 +48,6 @@
     siteIstotope();
   });
 
-  var siteOwlCarousel = function() {
-    $('.testimonial-carousel').owlCarousel({
-      center: true,
-      items: 1,
-      loop: true,
-      margin: 0,
-      autoplay: true,
-      smartSpeed: 1000,
-    });
-  };
-  siteOwlCarousel();
-
   $(window).on('load', function() {
     AOS.init({
       easing: 'ease',
